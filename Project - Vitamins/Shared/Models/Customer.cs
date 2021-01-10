e10e37ac2ecc4c39744c38d8f010a8efda6149ff
8-2018 <glavonjic1504@gmail.com>
2021-01-10 14:00:31 +0100

    Kreirane model-klase za svaku tabelu u bazi.

---
+++ Project - Vitamins/Shared/Models/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Models
+{
+    public class Customer
+    {
+        public int CustomerID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Adress { get; set; }
+        public string City { get; set; }
+        public string PhoneNumber { get; set; }
+        public string email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Project - Vitamins/Shared/Models/OrderItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Models
+{
+    public class OrderItem
+    {
+        public int OrderID { get; set; }
+        public int VitaminID { get; set; }
+        public int Quantity { get; set; }
+    }
+}
