54e614ac521c46b84f03fb41ff9cc00a4432a8c4
Jelena Djinovic <jelenadjinovic99@gmail.com>
2021-01-12 01:18:24 +0100

    Kreirane Business klase sa odgovarajucim metodama, kao i odgovarajuca povezivanja za komunikaciju sa Data slojem, preko pomocnog sloja, po principu DI.

---
+++ Project - Vitamins/BusinessLayer/OrderBusiness.cs
@@ -0,0 +1,29 @@
+﻿using Shared.Interfaces.Business;
+using Shared.Interfaces.Repository;
+using Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class OrderBusiness : IOrderBusiness
+    {
+        private readonly IOrderRepository orderRepository;
+
+        public OrderBusiness(IOrderRepository _orderRepository)
+        {
+            this.orderRepository = _orderRepository;
+        }
+        public bool AddNewOrder(Order order)
+        {
+            if (this.orderRepository.AddNewOrder(order) > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
